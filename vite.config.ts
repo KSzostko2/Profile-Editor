67633151b37933211adcbb6a475d93af41a4a94d
Kuba <kubaszostko@gmail.com>
2025-07-02 21:32:35 +0200

    Initial setup

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+/// <reference types="vitest" />
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    viteReact(),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+})
